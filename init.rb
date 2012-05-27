f60c13a34a94871f5864a012289e213582e60515
Onur Küçük <onur@delipenguen.net>
2012-05-27 13:19:41 +0300

    new features so new version

--- init.rb
+++ init.rb
@@ -7,7 +7,7 @@ Redmine::Plugin.register :redmine_mail_shaper do
   name 'Redmine Mail Shaper plugin'
   author 'Onur Küçük'
   description 'Format and behaviour changer plugin for Redmine notification e-mails '
-  version '0.1.0'
+  version '0.2.0'
   url 'http://www.ozguryazilim.com.tr'
   author_url 'http://www.ozguryazilim.com.tr'
   requires_redmine :version_or_higher => '1.4.0' # not tested with versions below
