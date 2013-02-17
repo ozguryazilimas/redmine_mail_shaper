664ea905823515e45c33e0a8113da0785089732a
Onur Küçük <onur@delipenguen.net>
2013-02-17 02:43:40 +0200

    update init for new redmine

--- init.rb
+++ init.rb
@@ -5,12 +5,12 @@ require 'unified_diff'
 
 Redmine::Plugin.register :redmine_mail_shaper do
   name 'Redmine Mail Shaper plugin'
-  author 'Onur Küçük'
+  author 'Onur Kucuk'
   description 'Format and behaviour changer plugin for Redmine notification e-mails '
   version '1.0.0'
   url 'http://www.ozguryazilim.com.tr'
   author_url 'http://www.ozguryazilim.com.tr'
-  requires_redmine :version_or_higher => '1.4.0' # not tested with versions below
+  requires_redmine :version_or_higher => '2.2.3' # not tested with versions below
 
   settings :partial => 'redmine_mail_shaper/settings',
     :default => {
@@ -24,9 +24,7 @@ Redmine::Plugin.register :redmine_mail_shaper do
 end
 
 
-require 'dispatcher'
-
-Dispatcher.to_prepare :redmine_mail_shaper do
+Rails.configuration.to_prepare do
   unless TimeEntry.included_modules.include?(RedmineMailShaper::Patches::TimeEntryMailShaperPatch)
     TimeEntry.send(:include, RedmineMailShaper::Patches::TimeEntryMailShaperPatch)
   end
