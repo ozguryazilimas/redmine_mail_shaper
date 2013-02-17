f3f26030ce2b2c4bf76875b7db27f488578c51b2
Onur Küçük <onur@delipenguen.net>
2013-02-17 22:15:04 +0200

    mark custom html created in helper as safe

--- lib/redmine_mail_shaper/patches/issues_helper_mail_shaper_patch.rb
+++ lib/redmine_mail_shaper/patches/issues_helper_mail_shaper_patch.rb
@@ -38,16 +38,16 @@ module RedmineMailShaper
 
               link_value = l(:label_f_hour_plural, :value => entry_attrs['hours'])
               link_value += no_html ? " #{entry_attrs['activity_name']}" :
-                                      " <i>#{entry_attrs['activity_name']}</i>"
+                                      " <i>#{entry_attrs['activity_name']}</i>".html_safe
               link_value += entry_attrs['comments']
 
               if detail.value == 'update'
                 link_value_was = l(:label_f_hour_plural, :value => entry_attrs['hours_was'])
                 link_value_was += no_html ? " #{entry_attrs['activity_name_was']}" :
-                                            " <i>#{entry_attrs['activity_name_was']}</i>"
+                                            " <i>#{entry_attrs['activity_name_was']}</i>".html_safe
                 link_value_was += entry_attrs['comments_was']
 
-                link_value = "#{link_value_was} -> #{link_value}"
+                link_value = "#{link_value_was} -> #{link_value}".html_safe
               end
 
               if time_entry
@@ -60,11 +60,11 @@ module RedmineMailShaper
                                   )
               else
                 time_entry_link = no_html ? "#{link_value} #{l(:label_deleted)}" :
-                                           "<strike><i>#{link_value} #{l(:label_deleted)}</i></strike>"
+                                           "<strike><i>#{link_value} #{l(:label_deleted)}</i></strike>".html_safe
               end
 
-              rval = no_html ? ["#{time_entry_change} #{time_entry_link}"] :
-                               ["<strong>#{time_entry_change}</strong> #{time_entry_link}"]
+              rval = no_html ? "#{time_entry_change} #{time_entry_link}" :
+                               "<strong>#{time_entry_change}</strong> #{time_entry_link}".html_safe
             end
           else
             # TODO: remove it when we switch to ruby 1.9.x
