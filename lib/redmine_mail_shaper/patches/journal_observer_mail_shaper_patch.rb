90126867fcbfd4833d60bafdadbb7961ad00bd74
Onur Küçük <onur@delipenguen.net>
2013-03-17 23:25:44 +0200

    Do not send email if issue has no comment change, has only one detail change, and the detail is set in plugin settings

--- lib/redmine_mail_shaper/patches/journal_observer_mail_shaper_patch.rb
+++ lib/redmine_mail_shaper/patches/journal_observer_mail_shaper_patch.rb
@@ -23,8 +23,37 @@ module RedmineMailShaper
                 (Setting.notified_events.include?('issue_priority_updated') && journal.new_value_for('priority_id').present?)
               )
             # Mailer.issue_edit(journal).deliver
-            Mailer.mail_shaper_deliver_issue_edit(journal)
+            Mailer.mail_shaper_deliver_issue_edit(journal) unless should_not_send_email(journal)
           end
+        end
+
+
+        private
+
+        def should_not_send_email(journal)
+          ret = false
+          settings = RedmineMailShaper.settings
+
+          if journal.notes.blank? and (journal.details.count == 1)
+            detail = journal.details.first
+            Rails.logger.debug "should_not_send_email working on JournalDetail: #{detail.id}"
+
+            case detail.property
+            when 'attachment'
+              ret = settings[:suppress_email_for_attachment]
+            when 'time_entry'
+              ret = settings[:suppress_email_for_time_entry]
+            when 'attr'
+              ret = settings[:suppress_email_for_attr].include? detail.prop_key
+            when 'cf'
+              custom_field = CustomField.find_by_id(detail.prop_key)
+              ret = settings[:suppress_email_for_cf].include? custom_field.name
+            end
+
+          end
+
+          Rails.logger.debug "should_not_send_email returns #{ret.inspect}"
+          ret
         end
 
       end
