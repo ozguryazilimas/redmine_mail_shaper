00a623decfdc57b159dbfdcd46c864736300a074
Onur Küçük <onur@delipenguen.net>
2014-10-19 23:27:30 +0300

    bump version to 2.3.1

--- init.rb
+++ init.rb
@@ -7,7 +7,7 @@ Redmine::Plugin.register :redmine_mail_shaper do
   name 'Redmine Mail Shaper plugin'
   author 'Onur Kucuk'
   description 'Format and behaviour changer plugin for Redmine notification e-mails '
-  version '2.3.0'
+  version '2.3.1'
   url 'http://www.ozguryazilim.com.tr'
   author_url 'http://www.ozguryazilim.com.tr'
   requires_redmine :version_or_higher => '2.5.1'
