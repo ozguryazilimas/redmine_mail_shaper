89df753b1d8f51fc805cf043aa67587e44b9a0a8
Onur Küçük <onur@delipenguen.net>
2015-04-08 01:48:11 +0300

    refs #15617 bump version for time entry settings

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_mail_shaper do
   name 'Redmine Mail Shaper plugin'
   author 'Onur Kucuk'
   description 'Format and behaviour changer plugin for Redmine notification e-mails '
-  version '2.7.0'
+  version '2.8.0'
   url 'http://www.ozguryazilim.com.tr'
   author_url 'http://www.ozguryazilim.com.tr'
   requires_redmine :version_or_higher => '2.5.1'
