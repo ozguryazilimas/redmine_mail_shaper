882f00b0f07410479ff944cea87d4c4e790be1cd
Onur Küçük <onur@delipenguen.net>
2015-07-14 01:05:05 +0300

    refs #16781 use view_time_entries permission if that strange plugin is not installed

--- lib/redmine_mail_shaper/patches/user_mail_shaper_patch.rb
+++ lib/redmine_mail_shaper/patches/user_mail_shaper_patch.rb
@@ -15,15 +15,16 @@ module RedmineMailShaper
       module InstanceMethods
 
         def rms_can_view_estimated_time(perm_project)
-          has_perm = true
-
           # check if redmine_hide_estimated_hours_permission plugin is used
-          # that plugin has weird permission to 'hide' a permission
+          # that plugin has weird permission to 'hide' a permission. Alternatively
+          # if redmine_hide_estimated_hours plugin is used, it uses view_time_entries
+          # permission that is already in redmine. If none of these are used, still
+          # view_time_entries is a good place to decide on permissions
           if respond_to?(:deny_view_estimated_time?)
-            has_perm = admin? || !allowed_to?(:hide_estimated_time, perm_project)
+            !allowed_to?(:hide_estimated_time, perm_project)
+          else
+            allowed_to?(:view_time_entries, perm_project)
           end
-
-          has_perm
         end
 
         def rms_can_view_time_entries(perm_project)
