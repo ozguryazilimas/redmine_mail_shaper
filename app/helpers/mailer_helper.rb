b5c0eb97b2c20cee6831383dcd6d7c05c7b50369
Onur Küçük <onur@delipenguen.net>
2016-03-06 16:21:40 +0200

    refs #18990 handlw wiki diff containing html tags especially span as it can cause failures

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -1,19 +1,20 @@
 module MailerHelper
   include ApplicationHelper
 
-  def truncate_for_mail_shaper(str_full)
+  def truncate_for_mail_shaper(str_full, is_text = false)
     max_lines = [Setting[:plugin_redmine_mail_shaper][:diff_max_lines_displayed].to_i, 0].max
     newline_seperator = "\r\n"
 
-    unless max_lines == 0
+    if is_text || max_lines == 0
+      str_truncated = str_full
+    else
       str_truncated = str_full.split(newline_seperator, max_lines+1)[0...max_lines].join(newline_seperator)
       truncated = (str_full.count(newline_seperator) > max_lines)
       span_diff_size = str_truncated.scan('<span class="diff_').size - str_truncated.scan('</span>').size
 
-      str_truncated << newline_seperator << '</span>' * span_diff_size
+      str_truncated << newline_seperator
+      str_truncated << '</span>' * span_diff_size if span_diff_size > 0
       str_truncated << newline_seperator << l(:text_diff_truncated) if truncated
-    else
-      str_truncated = str_full
     end
 
     str_truncated.html_safe
